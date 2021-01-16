4dbfbb39d275bb44b0c8e132662abfd63addce16
xjh22222228 <xjh22222228@gmail.com>
2021-01-16 19:24:47 +0800

    feat: grandom.Color

---
+++ grandom/color_test.go
@@ -0,0 +1,35 @@
+package grandom
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestColor(t *testing.T) {
+	_assert := assert.New(t)
+
+	// RGB
+	_assert.True(strings.HasPrefix(Color(C_RGB), "rgb("))
+	_assert.Equal(3, len(strings.Split(Color(C_RGB), ",")))
+
+	// RGBA
+	_assert.True(strings.HasPrefix(Color(C_RGBA), "rgba("))
+	_assert.Equal(4, len(strings.Split(Color(C_RGBA), ",")))
+
+	// HEX
+	_assert.Equal(7, len(Color(C_HEX)))
+	_assert.True(strings.HasPrefix(Color(C_HEX), "#"))
+
+	fmt.Println(Color(C_HEX))
+}
+
+func BenchmarkColor(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Color(C_RGB)
+		Color(C_RGBA)
+		Color(C_HEX)
+		Color(C_COLOR)
+	}
+}
